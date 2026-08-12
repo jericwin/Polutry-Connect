3201b77a5f8de6061ea3f08ca27a98cb54449c32
jericwin <punay.jeric@gmail.com>
2026-08-12 21:56:05 +0800

    Add Vercel deployment configuration

---
+++ api/index.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Add the root project directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import create_app
+
+app = create_app()
